60bab9f3da2a70a23dd03542cb88938137fd7f22
Chris Proctor <chris.proctor@gmail.com>
2015-05-08 14:03:52 -0700

    Requiring pyobjc. Adding architecture flag to installation instructions. Bumping to v 0.0.3

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
     packages = find_packages(),
     py_modules = ['nxtbrick'],
     scripts = ['scratch_nxt_helper'],
-    install_requires = ['flask', 'nxt-python', 'lightblue'],
+    install_requires = ['pyobjc', 'flask', 'nxt-python', 'lightblue'],
     package_data = {
         '': ['COPYING.txt', 'LICENSE', 'README.md', 'setup.cfg']
     },
-    version = '0.0.2',
+    version = '0.0.3',
     description = 'Provides a http helper app to allow Scratch 2 to control a LEGO NXT brick',
     author='Chris Proctor',
     author_email='chris.proctor@gmail.com',
